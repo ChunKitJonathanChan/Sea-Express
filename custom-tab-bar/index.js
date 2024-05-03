f9a01415c15099ffdc27208013e87874d55a8c8a
ChunKitJonathanChan <kitcck214@gmail.com>
2024-05-03 14:17:22 -0700

    fail

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,19 +1,19 @@
-// custom-tab-bar/index.js
+// pages/custom-tab-bar/index.js
 Component({
   data: {
     selected: 0,
     color: "#7A7E83",
-    selectedColor: "#3d44ff",
+    selectedColor: "#3cc51f",
     list: [{
-      pagePath: "/pages/index/index",
-      iconPath: "/images/tabbar_home.png",
-      selectedIconPath: "/images/tabbar_home_HL.png",
-      text: "home"
+      pagePath: "/index/index",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "组件"
     }, {
-      pagePath: "/pages/logins/login",
-      iconPath: "/images/tabbar_login.png",
-      selectedIconPath: "/images/tabbar_login_HL.png",
-      text: "login"
+      pagePath: "/index/index2",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
+      text: "接口"
     }]
   },
   attached() {
