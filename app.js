d24b629dc726557f55c9b562d64e1446f61e80bd
ChunKitJonathanChan <kitcck214@gmail.com>
2024-05-07 23:06:38 -0700

    minor change in custom form page

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ App({
       success: res => {
 
         console.log("code: ", res.code);
-        
+
         if (res.code) {
           wx.request({
             url: 'https://vmgzuhs2ta.execute-api.us-west-2.amazonaws.com/default/sea_express_login',
